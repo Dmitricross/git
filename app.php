51f22d99f081ad9cf2931d58c4958cde88bf2fa4
dmikab <dmikab@greeny.cs.tlu.ee>
2016-02-18 11:20:54 +0200

    Added another field

--- app.php
+++ app.php
@@ -1,6 +1,10 @@
 <?php
 
+//Getting the message from address
 
+$my_message = $_GET["message"];
+
+echo "My message is ".$my_message;
 
 ?>
 
@@ -9,6 +13,12 @@
 <form method="get">
 <label for="message">Message:*<label><br>
 <input type="text" name="message"><br>
+<!--This is the save button -->
+<input type="submit" value="Save to DB"
+
+
+<label for="message">To<label><br>
+<input type="text" name="romil"><br>
 <input type="submit" value="Save to DB"
 
 
