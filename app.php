f9d0f90b6df47b6e462cb2318e0f367875634ca8
dmikab <dmikab@greeny.cs.tlu.ee>
2016-02-18 12:09:46 +0200

    added from

--- app.php
+++ app.php
@@ -3,8 +3,14 @@
 //Getting the message from address
 
 $my_message = $_GET["message"];
+$to = $_GET["to"];
 
-echo "My message is ".$my_message;
+echo "My message is ".$my_message;" and is to ".$to; " and is to ".$from;
+
+
+
+
+
 
 ?>
 
@@ -12,13 +18,15 @@ echo "My message is ".$my_message;
 
 <form method="get">
 <label for="message">Message:*<label><br>
-<input type="text" name="message"><br>
+<input type="text" name="message"><br><br>
+
+<label for="from">from<label><br>
+<input type="text" name="from"><br><br>
+
+
+<label for="to">To<label><br>
+<input type="text" name="to"><br><br>
 <!--This is the save button -->
-<input type="submit" value="Save to DB"
-
-
-<label for="message">To<label><br>
-<input type="text" name="romil"><br>
 <input type="submit" value="Save to DB"
 
 
